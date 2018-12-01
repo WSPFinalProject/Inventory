cf9bae7729014ecebb46fe29bd3326ac45f4dca9
nkokkirala <43920690+nkokkirala@users.noreply.github.com>
2018-12-01 12:24:35 -0500

    Add files via upload

---
+++ src/Controller.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectwsp;
+
+/**
+ *
+ * @author nikhitak
+ */
+public class Controller {
+    private static Model m;
+    
+    public Controller(Model model)
+    {
+        m = model;
+        m.database();
+    }
+    
+    public void btnLogin(View v){
+        
+    }
+    public void btnSignUp(View v){
+        
+    }
+    public void btnAddBook(View v){
+        
+    }
+    public void btnDeleteBook(View v){
+        
+    }
+    public void btnUpdateBook(View v){
+        
+    }
+    public void btnCheckOutBook(View){
+        
+    }
+    public int decrementBooks(int numOfBooks, int booksBought){
+        if(/*name of book = name of book bought*/){
+                numOfBooks = numbOfBooks-booksBought;
+       
+    }
+    
+}
