223824de1275589ebf9111f14b71d9dd5a20d9fc
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-12-02 16:56:52 -0500

    Update User_bean.java

--- src/User_bean.java
+++ src/User_bean.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package csc4380.finalproject;
+package finalprojectwspt;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
