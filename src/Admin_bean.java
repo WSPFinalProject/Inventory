6d586d6cc5c516faf72bf54b99e5bafa3bd718ea
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-12-01 22:51:58 -0500

    Added Admin Functions

--- src/Admin_bean.java
+++ src/Admin_bean.java
@@ -63,4 +63,152 @@ public class Admin_bean {
 
         return results;
     }
+
+    public static String editBook(int bookID, String title, String author, String genre, String pub, int quantity) {
+        String status = "";
+
+        try {
+            st = con.createStatement();
+            st.executeUpdate("UPDATE book SET Title = \""+title+"\", Author = '"+author+"', Genre = '"+genre+"', Publisher = \""+pub+"\", Quantity = "+quantity+" WHERE BookID = "+bookID+"");
+            status = "Update Successful";
+
+        } catch (SQLException ex) {
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Update Failed");
+               status = "Update Failed";
+        }
+
+        return status;
+    }
+
+    public static String addBook(String title, String author, String genre, String pub, int quantity) {
+        String status = "";
+
+        try {
+            st = con.createStatement();
+            if(!bookExists(title, author, genre, pub)) {
+                st.executeUpdate("INSERT INTO book SET Title = \""+title+"\", Author = '"+author+"', Genre = '"+genre+"', Publisher = \""+pub+"\", Quantity = "+quantity+"");
+                status = "Add Book Successful";
+            } else {
+                status = "Add Book Failed";
+            }
+
+        } catch (SQLException ex) {
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Add Book Failed");
+               status = "Add Book Failed";
+        }
+
+        return status;
+    }
+
+    public static boolean bookExists(String title, String author, String genre, String pub) {
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM book WHERE Title = \""+title+"\" AND Author = '"+author+"' AND Genre = '"+genre+"' AND Publisher = \""+pub+"\"");
+
+            if(rs.next()) {
+                System.out.println("Book Already Exists");
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (SQLException ex) {
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Book Check Failed");
+               return false;
+        }
+    }
+
+    public static String deleteBook(int bookID) {
+        String status = "";
+
+        try {
+            st = con.createStatement();
+            st.executeUpdate("DELETE FROM book WHERE BookID = "+bookID+"");
+            status = "Delete Book Successful";
+
+        } catch (SQLException ex) {
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Delete Book Failed");
+               status = "Delete Book Failed";
+        }
+
+        return status;
+    }
+
+    public static String[][] checkByAuthor(String author) {
+        String[][] results = {};
+        String temp = "";
+        ArrayList<String[]> s = new ArrayList<String[]>();
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM book WHERE Author LIKE \'%"+author+"%\'");
+
+            for(int i = 0; rs.next(); i++) {
+                for(int j = 1; j <= 6; j++) {
+                    temp += rs.getString(j)+"@";
+                }
+                s.add(temp.split("@"));
+                temp = "";
+            }
+
+            results = new String[s.size()][6];
+
+            for(int i = 0; i < s.size(); i++) {
+                for(int j = 0; j < 6; j++) {
+                    results[i][j] = s.get(i)[j];
+                }
+            }
+
+            System.out.println(""+s.size()+" books found with author containing "+author+".");
+
+        } catch (SQLException ex) {
+            //Logger lgr = Logger.getLogger(Version.class.getName());
+            //lgr.log(Level.SEVERE, ex.getMessage(), ex);
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Check By Author Failed");
+        }
+
+        return results;
+    }
+
+    public static String[][] checkByTitle(String title) {
+        String[][] results = {};
+        String temp = "";
+        ArrayList<String[]> s = new ArrayList<String[]>();
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM book WHERE Title LIKE \'%"+title+"%\'");
+
+            for(int i = 0; rs.next(); i++) {
+                for(int j = 1; j <= 6; j++) {
+                    temp += rs.getString(j)+"@";
+                }
+                s.add(temp.split("@"));
+                temp = "";
+            }
+
+            results = new String[s.size()][6];
+
+            for(int i = 0; i < s.size(); i++) {
+                for(int j = 0; j < 6; j++) {
+                    results[i][j] = s.get(i)[j];
+                }
+            }
+
+            System.out.println(""+s.size()+" books found with title containing "+title+".");
+
+        } catch (SQLException ex) {
+            //Logger lgr = Logger.getLogger(Version.class.getName());
+            //lgr.log(Level.SEVERE, ex.getMessage(), ex);
+               Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+               System.out.println("Check By Title Failed");
+        }
+
+        return results;
+    }
 }
