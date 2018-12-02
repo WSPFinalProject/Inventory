aee3024ba5b4c7cb1030687ab5c232be44737e9a
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-12-02 14:04:54 -0500

    Add basic User functionality

--- src/User_bean.java
+++ src/User_bean.java
@@ -22,14 +22,14 @@ public class User_bean {
     private String  jdbc_drivers, url, user, password = "";
     private String current_user;
     private static String status;
-    
+
     public User_bean(Connection c) {
         con = c;
     }
-    
+
     public static String signup(String uname, String pass) {
         status = "";
-        
+
         try {
             st = con.createStatement();
             if(!userExists(uname)) {
@@ -38,17 +38,17 @@ public class User_bean {
             } else {
                 status = "User Already Exists";
             }
-            
-            
+
+
         } catch (SQLException ex) {
                Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
                System.out.println("Signup Failed");
                status = "Signup Failed";
         }
-        
+
         return status;
     }
-    
+
     public static boolean userExists(String uname) {
         try {
             st = con.createStatement();
@@ -57,22 +57,22 @@ public class User_bean {
                 return true;
             else
                 return false;
-            
+
         } catch (SQLException ex) {
                Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
                System.out.println("User Check Failed");
         }
-        
+
         return false;
     }
-    
+
     public static String login(String uname, String pass) {
         status = "";
-        
+
         try {
             st = con.createStatement();
             rs = st.executeQuery("SELECT * FROM users WHERE Username = '"+uname+"' AND Password = '"+pass+"'");
-            
+
             if(rs.next()) {
                 if(uname.equals("admin")) {
                     status = "Admin Login Successful";
@@ -82,20 +82,134 @@ public class User_bean {
             } else {
                 status = "Login Failed";
             }
-            
+
         } catch (SQLException ex) {
                Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
                System.out.println("Login Failed");
                status = "Login Failed";
         }
-        
+
         return status;
     }
     void setStatus(String s) {
         status = s;
     }
-    
+
     String getStatus() {
+        return status;
+    }
+
+    public static int getCurrentUser() {
+        return current_user;
+    }
+
+    public static String[][] getAvailableBooks() {
+        String[][] results = {};
+        String temp = "";
+        ArrayList<String[]> s = new ArrayList<String[]>();
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM book");
+
+            for(int i = 0; rs.next(); i++) {
+                if(Integer.parseInt(rs.getString(6)) > 0) {
+                    for(int j = 1; j <= 6; j++) {
+                        temp += rs.getString(j)+"@";
+                    }
+                    s.add(temp.split("@"));
+                    temp = "";
+                }
+            }
+
+            results = new String[s.size()][6];
+
+            for(int i = 0; i < s.size(); i++) {
+                for(int j = 0; j < 6; j++) {
+                    results[i][j] = s.get(i)[j];
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Get Available Books Failed");
+        }
+
+        return results;
+    }
+
+    public static String addToCart(int bookID, int quantity) {
+        String status = "";
+        int slot = getNextCartSlot();
+
+        if(slot > 0) {
+            try {
+            st = con.createStatement();
+            st.executeUpdate("UPDATE cart SET Book"+slot+" = "+bookID+", B"+slot+"Quan = "+quantity+" WHERE UserID = "+current_user+"");
+            status = "Add to Cart Successful";
+
+            } catch (SQLException ex) {
+                Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+                System.out.println("Add to Cart Failed");
+                status = "Add to Cart Successful";
+            }
+        }
+
+        return status;
+    }
+
+    public static int getNextCartSlot() {
+        int slot = -1;
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM cart WHERE UserID = "+current_user+"");
+
+            if(rs.next()) {
+                for(int i = 2; i <= 6; i++) {
+                    if(rs.getString(i) == null) {
+                        slot = i-1;
+                        System.out.println("Next Cart Slot is "+(i-1));
+                        break;
+                    }
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Get Cart Slot Failed");
+        }
+
+
+        return slot;
+    }
+
+    public static boolean isEnough(int bookID, int quantity) {
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT * FROM book WHERE BookID = "+bookID+"");
+
+            if(rs.next()) {
+                if(Integer.parseInt(rs.getString(6)) >= quantity) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Get Enoguh Books Failed");
+            return false;
+        }
+        return false;
+    }
+
+    public static String removeFromCart() {
+        String status = "";
+
+
+
         return status;
     }
 }
