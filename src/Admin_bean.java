f04bce5e68fe6ab192cee00239641b8d6cbd8479
mnarine1 <38436299+mnarine1@users.noreply.github.com>
2018-12-01 21:29:26 -0500

    Update Admin_bean.java

--- src/Admin_bean.java
+++ src/Admin_bean.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package finalprojectwsp;
-
+//test comment
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -24,20 +24,20 @@ public class Admin_bean {
     private String  jdbc_drivers, url, user, password = "";
     private String current_user, current_native, current_lastConver;
     private String status;
-    
+
     public Admin_bean(Connection c) {
         con = c;
     }
-    
+
     public static String[][] getInventory() {
         String[][] results = {};
         String temp = "";
         ArrayList<String[]> s = new ArrayList<String[]>();
-        
+
         try {
             st = con.createStatement();
             rs = st.executeQuery("SELECT * FROM book");
-            
+
             for(int i = 0; rs.next(); i++) {
                 for(int j = 1; j <= 6; j++) {
                     temp += rs.getString(j)+"@";
@@ -45,9 +45,9 @@ public class Admin_bean {
                 s.add(temp.split("@"));
                 temp = "";
             }
-            
+
             results = new String[s.size()][6];
-            
+
             for(int i = 0; i < s.size(); i++) {
                 for(int j = 0; j < 6; j++) {
                     results[i][j] = s.get(i)[j];
@@ -58,9 +58,9 @@ public class Admin_bean {
             //Logger lgr = Logger.getLogger(Version.class.getName());
             //lgr.log(Level.SEVERE, ex.getMessage(), ex);
                Logger.getLogger(Admin_bean.class.getName()).log(Level.SEVERE, null, ex);
-               System.out.println("Exception Caught");               
+               System.out.println("Exception Caught");
         }
-        
+
         return results;
     }
 }
